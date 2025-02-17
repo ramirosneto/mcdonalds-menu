f5f665231b081fd86efb750f31b05f98f2de09ce
ramirosneto <netosramiro@gmail.com>
2025-02-16 22:02:52 -0300

    adding bottomsheet compose

--- app/src/main/java/br/com/mcdonalds/menu/ui/McDonaldsMenuApp.kt
+++ app/src/main/java/br/com/mcdonalds/menu/ui/McDonaldsMenuApp.kt
@@ -3,30 +3,42 @@ package br.com.mcdonalds.menu.ui
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -35,7 +47,7 @@ import androidx.compose.ui.unit.sp
 import br.com.mcdonalds.menu.R
 import br.com.mcdonalds.menu.model.Menu
 import br.com.mcdonalds.menu.model.MenuItem
-import br.com.mcdonalds.menu.ui.view.MenuItemBottomSheet
+import br.com.mcdonalds.menu.utils.CurrencyUtils
 import br.com.mcdonalds.menu.viewmodel.MenuViewModel
 import coil.compose.rememberAsyncImagePainter
 
@@ -124,16 +136,19 @@ fun HorizontalList(menu: Menu) {
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ItemCard(menuItem: MenuItem) {
+    var showSheet by remember { mutableStateOf(false) }
+
     Card(
         border = BorderStroke(1.dp, Color.Gray),
         colors = CardColors(Color.White, Color.White, Color.White, Color.White),
         modifier = Modifier
             .padding(8.dp)
             .clickable {
-                val bottomSheet = MenuItemBottomSheet(menuItem)
-                //bottomSheet.show(supportFragmentManager, MenuItemBottomSheet.TAG)
+                showSheet = true
             }
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
@@ -155,6 +170,70 @@ fun ItemCard(menuItem: MenuItem) {
                 fontWeight = FontWeight.Light,
                 fontSize = 12.sp
             )
+        }
+    }
+
+    if (showSheet) {
+        val bottomSheetState =
+            rememberModalBottomSheetState(skipPartiallyExpanded = true)
+
+        ModalBottomSheet(
+            sheetState = bottomSheetState,
+            onDismissRequest = {
+                showSheet = false
+            }
+        ) {
+            Column(
+                modifier = Modifier
+                    .background(Color.White)
+                    .padding(16.dp)
+            ) {
+                Image(
+                    painter = rememberAsyncImagePainter(model = menuItem.url),
+                    modifier = Modifier
+                        .width(390.dp)
+                        .padding(top = 32.dp)
+                        .align(Alignment.CenterHorizontally),
+                    contentDescription = menuItem.name
+                )
+                Text(
+                    text = menuItem.name,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 32.dp)
+                        .align(Alignment.CenterHorizontally),
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp
+                )
+                Text(
+                    text = CurrencyUtils.formatPrice(menuItem.price),
+                    modifier = Modifier
+                        .wrapContentWidth()
+                        .padding(top = 16.dp)
+                        .absolutePadding(16.dp)
+                        .align(Alignment.CenterHorizontally)
+                        .clip(RoundedCornerShape(10.dp))
+                        .border(BorderStroke(2.dp, SolidColor(Color.Black))),
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 16.sp,
+
+                    )
+                Text(
+                    text = menuItem.description,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp)
+                        .align(Alignment.CenterHorizontally),
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 16.sp
+                )
+            }
         }
     }
 }
--- app/src/main/java/br/com/mcdonalds/menu/ui/view/MenuItemBottomSheet.kt
+++ app/src/main/java/br/com/mcdonalds/menu/ui/view/MenuItemBottomSheet.kt
@@ -1,39 +1,105 @@
 package br.com.mcdonalds.menu.ui.view
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import br.com.mcdonalds.menu.R
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.mcdonalds.menu.model.MenuItem
-import br.com.mcdonalds.menu.databinding.ModalBottomSheetContentBinding
 import br.com.mcdonalds.menu.utils.CurrencyUtils
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.squareup.picasso.Picasso
+import coil.compose.rememberAsyncImagePainter
+import kotlinx.coroutines.launch
 
-class MenuItemBottomSheet(private val menuItem: MenuItem) : BottomSheetDialogFragment() {
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MenuItemBottomSheet(menuItem: MenuItem) {
+    val scope = rememberCoroutineScope()
+    val scaffoldState = rememberBottomSheetScaffoldState()
 
-    private lateinit var binding: ModalBottomSheetContentBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = ModalBottomSheetContentBinding.inflate(LayoutInflater.from(context), container, false)
-        setupBottomSheet()
-
-        return binding.root
-    }
-
-    private fun setupBottomSheet() {
-        Picasso.get().load(menuItem.url).into(binding.menuItemImg)
-        binding.menuItemText.text = menuItem.name
-        binding.menuItemPrice.text = CurrencyUtils.formatPrice(menuItem.price)
-        binding.menuItemDescription.text = menuItem.description
-    }
-
-    companion object {
-        const val TAG = "ModalBottomSheet"
+    BottomSheetScaffold(
+        scaffoldState = scaffoldState,
+        sheetContent = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = rememberAsyncImagePainter(model = menuItem.url),
+                    modifier = Modifier
+                        .size(100.dp)
+                        .align(Alignment.Center),
+                    contentDescription = menuItem.name
+                )
+                Text(
+                    text = menuItem.name,
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                        .width(120.dp)
+                        .align(Alignment.Center),
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 12.sp
+                )
+                Text(
+                    text = CurrencyUtils.formatPrice(menuItem.price),
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                        .width(120.dp)
+                        .align(Alignment.Center),
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 12.sp
+                )
+                Text(
+                    text = menuItem.description,
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                        .width(120.dp)
+                        .align(Alignment.Center),
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 12.sp
+                )
+            }
+        },
+        sheetPeekHeight = 0.dp,
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(onClick = {
+                scope.launch {
+                    scaffoldState.bottomSheetState.expand()
+                }
+            }) {
+                Text("Show Bottom Sheet")
+            }
+        }
     }
 }
